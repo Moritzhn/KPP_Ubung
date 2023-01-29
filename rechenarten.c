6280f9764f7d3688823f917d20c426e271fb1cdd
Moritzhn <119943162+Moritzhn@users.noreply.github.com>
2023-01-30 00:11:19 +0100

    Create rechenarten.c

---
+++ rechenarten.c
@@ -0,0 +1,17 @@
+include<stdio.h>
+
+float summe(eins, zwei){
+return eins + zwei;
+}
+
+float prdukt(eins, zwei){
+return eins * zwei;
+}
+
+float differenz{
+return eins - zwei;
+}
+
+float qoutient{
+return eins / zwei;
+}
