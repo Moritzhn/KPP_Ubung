f7536614451ed3da7718ed6833dfff7533ce3298
Moritzhn <119943162+Moritzhn@users.noreply.github.com>
2023-01-30 00:43:05 +0100

    Update rechenarten.c

--- rechenarten.c
+++ rechenarten.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 
-float summe(eins, zwei){
+int summe(eins, zwei){
 return eins + zwei;
 }
 
-float produkt(eins, zwei){
+int produkt(eins, zwei){
 return eins * zwei;
 }
 
-float differenz(eins, zwei){
+int differenz(eins, zwei){
 return eins - zwei;
 }
 
-float qoutient(eins, zwei){
+int qoutient(eins, zwei){
 return eins / zwei;
 }
