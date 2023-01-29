5975750db2ccedc192662b7351c09a2ce3763086
Moritzhn <119943162+Moritzhn@users.noreply.github.com>
2023-01-30 00:08:04 +0100

    Create taschenrechner.c

---
+++ taschenrechner.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<rechenarten.h>
+
+int main(){
+	
+	float eins, zwei;
+	printf("Geben sie die erste Zahl ein:");
+	scanf("%f", &eins);
+  printf("Geben sie die zweite Zahl ein:");
+	scanf("%f", &zwei);
+	
+	printf("Die Summe der Zahlen lautet: %f \n", summe(eins, zwei));
+	printf("Das Produkt der Zahlen lautet: %f \n", produkt(eins, zwei));
+	printf("Die Differenz der Zahlen lautet: %f \n", differenz(eins, zwei));
+	printf("Der Qoutient der Zahlen lautet: %f \n", qountient(eins, zwei));
+	
+}
